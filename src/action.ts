a9c0b95eaa398f7ab146dc4c82cc72e6fcbb77bc
Nathan McCorkle <nathan.mccorkle@relativity.com>
2025-01-08 13:52:36 -0600

    use conventionalcommits preset

--- src/action.ts
+++ src/action.ts
@@ -125,9 +125,10 @@ export default async function main() {
 
     let bump = await analyzeCommits(
       {
+        preset: 'conventionalcommits',
         releaseRules: mappedReleaseRules
           ? // analyzeCommits doesn't appreciate rules with a section /shrug
-            mappedReleaseRules.map(({ section, ...rest }) => ({ ...rest }))
+          mappedReleaseRules.map(({ section, ...rest }) => ({ ...rest }))
           : undefined,
       },
       { commits, logger: { log: console.info.bind(console) } }
